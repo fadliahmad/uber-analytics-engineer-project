15643a9241aa06e51a484e9fc3819a7974739ebf
fadliahmad <fadliahmad.j@gmail.com>
2024-02-12 19:55:03 +0700

    add mage-file

--- mage-files/extract.py
+++ mage-files/extract.py
@@ -12,7 +12,7 @@ def load_data_from_api(*args, **kwargs):
     """
     Template for loading data from API
     """
-    url = 'https://storage.googleapis.com/uber-data-engineering-project-darshil/uber_data.csv'
+    url = 'https://storage.googleapis.com/uber-analytics-engineer-project/uber_data.csv'
     response = requests.get(url)
 
     return pd.read_csv(io.StringIO(response.text), sep=',')
--- mage-files/load.py
+++ mage-files/load.py
@@ -1,4 +1,4 @@
-from mage_ai.data_preparation.repo_manager import get_repo_path
+from mage_ai.settings.repo import get_repo_path
 from mage_ai.io.bigquery import BigQuery
 from mage_ai.io.config import ConfigFileLoader
 from pandas import DataFrame
@@ -15,16 +15,16 @@ def export_data_to_big_query(data, **kwargs) -> None:
     Specify your configuration settings in 'io_config.yaml'.
 
     Docs: https://docs.mage.ai/design/data-loading#bigquery
-
-    
     """
+
     config_path = path.join(get_repo_path(), 'io_config.yaml')
     config_profile = 'default'
 
-    for key, value in data.items():
-        table_id = 'data-with-darshil.uber_data_engineering_yt.{}'.format(key)
+    for key, value in data.items(): 
+        table_id = 'technical-test-project.uber_dataset.{}'.format(key)
+
         BigQuery.with_config(ConfigFileLoader(config_path, config_profile)).export(
             DataFrame(value),
             table_id,
             if_exists='replace',  # Specify resolution policy if table name already exists
-        )
+    )
